eac3908be19802f4d5854e60930c60eeb1c946be
aniger429 <aniger429@gmail.com>
2016-08-01 00:44:29 +0800

    UI updates again :/
    alright, just simplified the layout scheme for the UIs.
    accounting manager is back.

--- project/resources/views/customer/transaction.blade.php
+++ project/resources/views/customer/transaction.blade.php
@@ -1,4 +1,4 @@
-@extends('customer/customerLayout')
+@extends('master_layout/customer_master')
 
 @section('customCSS')
     <link href="css/transaction.css" rel="stylesheet">
@@ -12,10 +12,11 @@
               <table class="table table-hover">
                   <thead>
                     <tr>
-                      <th class="col-md-6">Item</th>
+                      <th class="col-md-4">Item</th>
                       <th class="col-md-2">Price</th>
                       <th class="col-md-2">Quantity</th>
                       <th class="col-md-2">Subtotal</th>
+                      <th class="col-md-2">Review Product</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -24,18 +25,111 @@
                       <td>4000</td>
                       <td>2</td>
                       <td>8000</td>
+                      <td>
+                        <button type="button" class="btn btn-md btn-success"
+                        data-toggle="collapse" data-target="#reviewForm">
+                         Review
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colspan="5">
+                          <div id="reviewForm" class="collapse well">
+                            <form class="form-horizontal" role="form">
+                              <div class="form-group">
+                                <label for="review">Write Review:</label>
+                                <input type="review" class="form-control" id="review">
+                              </div>
+                              <div class="form-group">
+                                <label for="review">Rating:</label>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                              </div>
+                              <div class="form-group">
+                                <div class="pull-right">
+                                  <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
+                        </td>
                     </tr>
                     <tr>
                       <td>Sandals for the Spartan</td>
                       <td>1000</td>
                       <td>1</td>
                       <td>1000</td>
+                      <td>
+                        <button type="button" class="btn btn-md btn-success"
+                        data-toggle="collapse" data-target="#reviewForm1">
+                         Review
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colspan="5">
+                          <div id="reviewForm1" class="collapse well">
+                            <form class="form-horizontal" role="form">
+                              <div class="form-group">
+                                <label for="review">Write Review:</label>
+                                <input type="review" class="form-control" id="review">
+                              </div>
+                              <div class="form-group">
+                                <label for="review">Rating:</label>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                              </div>
+                              <div class="form-group">
+                                <div class="pull-right">
+                                  <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
+                        </td>
                     </tr>
                     <tr>
                       <td>Dollshoes for that ballerina</td>
                       <td>1500</td>
                       <td>3</td>
                       <td>4500</td>
+                      <td>
+                        <button type="button" class="btn btn-md btn-success"
+                        data-toggle="collapse" data-target="#reviewForm2">
+                         Review
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colspan="5">
+                          <div id="reviewForm2" class="collapse well">
+                            <form class="form-horizontal" role="form">
+                              <div class="form-group">
+                                <label for="review">Write Review:</label>
+                                <input type="review" class="form-control" id="review">
+                              </div>
+                              <div class="form-group">
+                                <label for="review">Rating:</label>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                              </div>
+                              <div class="form-group">
+                                <div class="pull-right">
+                                  <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
+                        </td>
                     </tr>
                      <tr>
                       <td></td>
@@ -49,13 +143,14 @@
           <hr/>
            <h1>July 22, 2016</h1>
           <div class="table-responsive">
-              <table class="table table-striped">
+              <table class="table table-hover">
                   <thead>
                     <tr>
-                      <th class="col-md-6">Item</th>
+                      <th class="col-md-4">Item</th>
                       <th class="col-md-2">Price</th>
                       <th class="col-md-2">Quantity</th>
                       <th class="col-md-2">Subtotal</th>
+                      <th class="col-md-2">Review Product</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -64,6 +159,11 @@
                       <td>2000</td>
                       <td>1</td>
                       <td>2000</td>
+                      <td>
+                        <button type="button" class="btn btn-md btn-success">
+                         Review
+                        </button>
+                      </td>
                     </tr>
                      <tr>
                       <td></td>
@@ -76,7 +176,6 @@
           </div>
       </div>
 <!-- /.container -->
-
 @endsection
 
 @section('customScripts')
